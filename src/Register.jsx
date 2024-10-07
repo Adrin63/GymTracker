8278603523476f3d008870ee9463d621db58d73c
Adrin63 <adrianriscolopez@gmail.com>
2024-10-07 23:34:07 +0200

    register completed + some little changes

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,27 +1,41 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register(){
     
+    const [allUsers, setAllUsers] = useState([]);
+
     useEffect(() => {
-        
-    })
+        fetch('/db.json')
+        .then(resp => resp.json())
+        .then(data => {
+            setAllUsers(data);
+        })
+    }, []);
 
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
 
+    const [status, setStatus] = useState("");
+
     const register = (e) => {
-        e.preventdefault();
+        e.preventDefault();
         console.log('submitea')
 
-        
+
     }
 
     return (
         <div className="bg-slate-700">
-            <button className="bg-orange-300 rounded-full flex items-center justify-center w-[50px] h-[50px] p-3">
-                X
-            </button>
+            <Link to='/'>
+                <button className="bg-orange-300 rounded-full flex items-center justify-center w-[50px] h-[50px] p-3 absolute top-5 left-5 text-white">
+                   <h1 style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}>X</h1>
+                </button>
+            </Link>
             <div className=" w-full h-screen flex flex-col items-center justify-center">
+                <h1 className="text-white font-bold italic text-2xl p-3">
+                    Crear Usuario
+                </h1>
                 <form onSubmit={register} className=" text-center space-y-10">
                     <div>
                         <label style={{ textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }} className="block text-gray-200 text-sm font-bold mb-2" htmlFor="nombre">
@@ -47,6 +61,8 @@ function Register(){
                         </button>
                     </div>
                 </form>
+
+                <h1 className="text-red-600 p-4">{status}</h1>
             </div>
         </div>
     
