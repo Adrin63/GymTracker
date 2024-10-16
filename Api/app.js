3e473b23a1dfd3a2e367ca988ef5f3dfc3d6f137
Adrin63 <adrianriscolopez@gmail.com>
2024-10-16 19:42:27 +0200

    updates!

--- Api/app.js
+++ Api/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
+const models = require("./models");
+
 app.get("/", function(req, res) {
+    console.log('Docker funciona')
     return res.send("funciona");
 });
+
+
 
 app.listen(3000, function(){
     console.log('API On, Port: 3000');
---
+++ Api/models.js
@@ -0,0 +1,107 @@
+const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const sequelize = new Sequelize('gymtracker', 'root', 'root', {
+    //host: 'localhost',
+    host: 'localhost', //IP bbdd
+    port: 3306,
+    dialect: 'mysql' // conecta a mysql
+});
+
+// Runear 1 vez, convierte las tablas del Sequelize a MySql
+// async function iniDB(){
+// await sequelize.sync({force: true});
+// console.log('tablas creadas');
+// }
+
+// iniDB();
+
+
+//DEFINIR TABLAS
+//DEFINIR TABLAS
+//DEFINIR TABLAS
+
+const AsistedDays = sequelize.define('asistedDays', {
+    date: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Users = sequelize.define('users', {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+});
+
+const Rutines = sequelize.define('rutines', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    color: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    image: {//URL
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+});
+
+const MuscularGroup = sequelize.define('muscularGroup', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    }
+});
+
+const Exercises = sequelize.define('exercises', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    info: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+    }
+});
+
+
+AsistedDays.belongsToMany(Users, {through: 'asistedDays_has_users'});
+Users.belongsToMany(AsistedDays, {through: 'asistedDays_has_users'});
+
+Users.hasMany(Rutines);
+Rutines.belongsTo(Users);
+
+AsistedDays.belongsTo(Rutines);
+Rutines.hasMany(AsistedDays);
+
+Rutines.belongsToMany(MuscularGroup, {through: 'rutines_has_musculargroup'});
+MuscularGroup.belongsToMany(Rutines, {through: 'rutines_has_musculargroup'});
+
+MuscularGroup.hasMany(Exercises);
+Exercises.belongsTo(MuscularGroup);
+
+Users.beforeCreate(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, 10); // Encripta la contrasenya amb bcrypt
+    user.password = hashedPassword;
+  });
+  
+
+module.exports = {
+AsistedDays,
+Users,
+Rutines,
+MuscularGroup,
+Exercises
+};
