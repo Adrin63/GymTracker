6a373ae0e40eabd178559408bfcbf12a7a5a7190
Adrin63 <adrianriscolopez@gmail.com>
2024-10-26 00:47:51 +0200

    improved layout exercisesrutineselector

--- Front/src/components/Exercises/NewExerciseTag.jsx
+++ Front/src/components/Exercises/NewExerciseTag.jsx
@@ -1,6 +1,6 @@
 function NewExerciseTag() {
     return (
-        <div className="p-1 bg-orange-300  rounded-lg w-1/12 flex items-center justify-center text-center">
+        <div className="p-1 bg-orange-300  rounded w-1/12 flex items-center justify-center text-center">
             <h3 className="text-center font-bold font-2xl text-slate-500">+</h3>
         </div>
     )
--- Front/src/pages/Rutines/ExercisesRutineSelector.jsx
+++ Front/src/pages/Rutines/ExercisesRutineSelector.jsx
@@ -2,11 +2,11 @@ import { useContext, useEffect, useState } from 'react';
 import Context from '../../Context';
 import NewExerciseTag from '../../components/Exercises/NewExerciseTag';
 
-function ExercisesRutineSelector(){
+function ExercisesRutineSelector() {
 
-    const {selectedMuscularGroups, selectedName, setSelectedName, selectedColor, setOnExercises, setSelectedMuscularGroups} = useContext(Context);
+    const { selectedMuscularGroups, selectedName, setSelectedName, selectedColor, setOnExercises, setSelectedMuscularGroups } = useContext(Context);
 
-    
+
     useEffect(() => {
         setOnExercises(true);
 
@@ -20,23 +20,35 @@ function ExercisesRutineSelector(){
         //     setSelectedName(newName);
         // }
     }, [])
-    
+
+    const changeBetweenLbsAndKg = (exercice) => {
+        console.log(exercice)
+    }
+
     return (
         <div className='flex flex-col'>
             {selectedMuscularGroups.map((musculparGroup, index) => (
                 <div className='flex flex-col p-3 space-y-4'>
                     <h1 key={index} className='text-white font-bold text-2xl uppercase'>{musculparGroup}</h1>
-                    <input
+                    <div className='flex flex-row space-x-2'>
+                        <input
                             //onInput={(e) => { if(e.target.value.length < 38) }}
                             value={""}
-                            className="appearance-none border rounded-xl w-full p-3 text-gray-700 leading-tight" id="name" type="text" placeholder={"Ejercicio 1"} />
-                        
-                        <div className='flex justify-center items-center'>
+                            className="appearance-none border rounded w-full p-3 text-gray-700 leading-tight" id="name" type="text" placeholder={"Ejercicio 1"} />
+                        <input
+                            //onInput={(e) => { if(e.target.value.length < 38) }}
+                            value={""}
+                            className="appearance-none border rounded w-1/6 p-3 text-gray-700 leading-tight text-center" id="name" type="text" placeholder={"10"} />
+                        <button className='flex justify-center items-center p-3 uppercase bg-orange-300 text-slate-500 font-bold rounded' onClick={() => changeBetweenLbsAndKg('20')}>
+                            kg
+                        </button>
+                    </div>
+                    <div className='flex justify-center items-center'>
 
-                        <NewExerciseTag/>
-                        </div>
+                        <NewExerciseTag />
+                    </div>
                 </div>
-                ))}
+            ))}
         </div>
     )
 }
