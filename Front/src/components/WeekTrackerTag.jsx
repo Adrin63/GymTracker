33312218c82ff67f80a21c8d8b8992bc34e88a07
Adrin63 <adrianriscolopez@gmail.com>
2024-11-18 18:31:44 +0100

    weektrackertag done

--- Front/src/components/WeekTrackerTag.jsx
+++ Front/src/components/WeekTrackerTag.jsx
@@ -1,7 +1,22 @@
-function WeekTrackerTag(progress, total = 0) {
+function WeekTrackerTag({ progress = 0, total = 0 }) {
     return (
-        <div className="flex flex-row p-1 bg-blue-400 rounded-lg">
-            
+        <div className="flex flex-row justify-between bg-blue-400 rounded-xl overflow-hidden">
+            {
+                (() => {
+                    const divs = [];
+                    for (let i = 0; i < total; i++) {
+                        divs.push(
+                            <div
+                                key={i}
+                                className={`py-2 w-full border border-black ${i < progress ? "bg-orange-300" : "bg-slate-300"
+                                    } ${i === 0 ? "rounded-l-xl" : i === total - 1 ? "rounded-r-xl" : ""
+                                    }`}
+                            />
+                        );
+                    }
+                    return divs;
+                })()
+            }
         </div>
     );
 }
